c5dfd04294e6de4d7255cfb1853527c96ebaab52
Rustem Kakimov <ruskakimov@gmail.com>
2024-07-20 17:11:40 -0400

    Create shopping cart activity

--- app/src/main/java/com/example/buttonmashers/MainActivity.kt
+++ app/src/main/java/com/example/buttonmashers/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.buttonmashers
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val profileBtn = findViewById<ImageButton>(R.id.profile_btn)
+        val cartBtn = findViewById<ImageButton>(R.id.cart_btn)
+
+        cartBtn.setOnClickListener {
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
         }
     }
 }
