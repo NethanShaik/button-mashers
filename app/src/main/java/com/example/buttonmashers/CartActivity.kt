3da500b043b09f5d95eb590fa563e27a9419cfff
NethanShaik <nethanshaik@gmail.com>
2024-07-22 16:01:09 -0400

    Cart activity code

--- app/src/main/java/com/example/buttonmashers/CartActivity.kt
+++ app/src/main/java/com/example/buttonmashers/CartActivity.kt
@@ -2,6 +2,9 @@ package com.example.buttonmashers
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +12,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CartActivity : AppCompatActivity() {
+    fun increase_quantity(quantity: Int): Int {
+        return quantity + 1
+    }
+
+    fun decrease_quantity(quantity: Int): Int {
+        return quantity - 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,12 +28,35 @@ class CartActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar) // Set the toolbar as the support action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // Show back button
         supportActionBar?.setDisplayShowTitleEnabled(true) // Show title
+
+        val quantity: EditText = findViewById(R.id.quantity)
+        val price: TextView = findViewById(R.id.price)
+        val increase: ImageButton = findViewById(R.id.increase)
+        val decrease: ImageButton = findViewById(R.id.decrease)
+
+        increase.setOnClickListener {
+
+            val quant: Int = quantity.text.toString().toIntOrNull() ?: 0
+            val increased_value = increase_quantity(quant)
+            quantity.setText(increased_value.toString())
+
+        }
+
+        decrease.setOnClickListener {
+
+            val quant: Int = quantity.text.toString().toIntOrNull() ?: 0
+            val decreased_value = decrease_quantity(quant)
+            quantity.setText(decreased_value.toString())
+
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
