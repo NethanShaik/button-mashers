36c9595f37200845d54ace5c32857d41cb5b7b5d
Carlos Ordoñez <70831953+Carlos-1609@users.noreply.github.com>
2024-07-24 16:46:03 -0400

    Increment Quantity
    Added the functionality for the + and - icons to increase and decreased the items wuantity

--- app/src/main/java/com/example/buttonmashers/ItemActivity.kt
+++ app/src/main/java/com/example/buttonmashers/ItemActivity.kt
@@ -2,6 +2,8 @@ package com.example.buttonmashers
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -9,20 +11,36 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ItemActivity : AppCompatActivity() {
+    private lateinit var textViewQuantity: TextView
+    private lateinit var iconDecrease : ImageButton
+    private var number: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_item)
+        val iconIncrease : ImageButton = findViewById(R.id.iconIncrease)
+        textViewQuantity = findViewById(R.id.textViewQuantity)
+        iconDecrease = findViewById(R.id.iconDecrease)
+        iconDecrease.isEnabled = false// Disable decrease button initially
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar) // Set the toolbar as the support action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // Show back button
         supportActionBar?.setDisplayShowTitleEnabled(true) // Show title
+
+        iconIncrease.setOnClickListener{
+            number++// Increase number quantity
+            updateNumber()
+        }
+
+        iconDecrease.setOnClickListener{
+            number--// Decrease number quantity
+            updateNumber()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -34,5 +52,11 @@ class ItemActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item) // Keep default behavior
         }
+    }
+
+    private fun updateNumber() {
+        // Update the text view with the current quantity
+        textViewQuantity.text = number.toString()
+        iconDecrease.isEnabled = number > 1// Disable decrease button if number is 1
     }
 }
